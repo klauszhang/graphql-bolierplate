b0cc9a12fd14086c3ac7752fedebe7e584410f06
hao <klaushaozhang@gmial.com>
2017-08-27 16:32:08 +1200

    add input query

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { createServer } = require('http');
-
+const ProductStore = require('./stores/ProductStore');
 const schema = require('./schema');
 
 const PORT = 3000;
+const productStore = new ProductStore();
 
 const start = async () => {
   const app = express();
 
-  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+  const options = {
+    context: {
+      productStore
+    },
+    schema
+  };
+
+  app.use('/graphql', bodyParser.json(), graphqlExpress(options));
   app.use(
     '/graphiql',
     graphiqlExpress({
--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,12 +1,14 @@
 module.exports = {
   Query: {
-    allProducts: async (root, data, context) => {
-      return [
-        {
-          id: '1',
-          name: 'hello'
-        }
-      ];
+    allProducts: async (root, data, { productStore }) => {
+      const { first, after } = data.page;
+      return productStore.findAll(first, after);
+    },
+    ProductById: async (_, { id }, { productStore }) => {
+      return productStore.findById(id);
+    },
+    ProductBySku: async (_, { sku }, { productStore }) => {
+      return productStore.findBySku(sku);
     }
   }
 };
